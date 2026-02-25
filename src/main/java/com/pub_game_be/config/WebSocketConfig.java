11d0565c660dbca94a2bc44d08b84e8da9a3b955
psiep8 <giusepperomagnoli808@gmail.com>
2026-02-25 22:12:47 +0100

    clean code...

--- src/main/java/com/pub_game_be/config/WebSocketConfig.java
+++ src/main/java/com/pub_game_be/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-pubgame")
-                .setAllowedOriginPatterns("*") // ✅ Già corretto
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
